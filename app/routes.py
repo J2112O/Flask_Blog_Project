c54db5b09e1e9299f313867b105885d8ab69f05e
Joshua Otwell <parabollus@gmail.combigdaddy@LE2>
2018-07-17 18:38:03 -0400

    Providing logout route, link and functionality for logged in users.

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Post
 from app import app, db, bcrypt
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 
 posts = [
     {
@@ -39,6 +39,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')  # decode() makes it a string and not in bytes
@@ -52,6 +54,8 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()  # Getting form data here remember
@@ -61,3 +65,9 @@ def login():
         else:
             flash('Login Unsuccessful. Please check email and/or password.', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
